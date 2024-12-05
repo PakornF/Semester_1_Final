44c5388a1ee529b3816af6822f452a48f7072511
Pakorn Fudulyawajananont <pakorn.f@ku.th>
2024-12-05 14:34:10 +0700

    CoinBankNoteApplication

---
+++ CoinBankNoteApp.py
@@ -0,0 +1,42 @@
+class Coin:
+
+    def __init__(self, value: int = 1):
+        self.value = value
+
+    def __str__(self):
+        return f"{self.value} Baht Coin."
+
+    def get_value(self):
+        return self.value
+
+    def set_value(self, new_value):
+        self.value = new_value
+
+class BankNote:
+
+    def __init__(self, value: int = 20):
+        self.value = value
+
+    def __str__(self):
+        return f"{self.value} Baht Banknote."
+
+    def get_value(self):
+        return self.value
+
+    def set_value(self, new_value):
+        self.value = new_value
+
+class CoinBankNoteApp:
+    denominations = [BankNote(1000), BankNote(500), BankNote(100),
+                     BankNote(50), BankNote(20), Coin(1),
+                     Coin(2), Coin(5), Coin(10)]
+
+    amount = int(input("Input amount : "))
+
+    for denom in denominations:
+        num = amount // denom.get_value()
+
+        if num > 0:
+            print(f"You get {num} of {denom}")
+
+        amount -= num * denom.get_value()
